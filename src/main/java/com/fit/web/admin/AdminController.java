9cb520b32f7810b10ffdff6604bea5453e0e3389
cyril314 <svip.cyril@gmail.com>
2026-01-09 11:49:14 +0800

    initial commit

--- src/main/java/com/fit/web/admin/AdminController.java
+++ src/main/java/com/fit/web/admin/AdminController.java
@@ -9,6 +9,7 @@ import com.fit.service.ZtreeNodeService;
 import com.fit.util.SystemUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -30,6 +32,8 @@ import java.util.List;
 @RequestMapping("/admin")
 public class AdminController extends BaseController {
 
+    @Value("${server.port}")
+    private String port = "80";
     @Autowired
     private MenuNodeService menuService;
     @Autowired
@@ -45,6 +49,8 @@ public class AdminController extends BaseController {
     @GetMapping("/welcome")
     public String main(Model model) {
         model.addAttribute("os", SystemUtil.getOsInfo());
+        model.addAttribute("port", port);
+        model.addAttribute("startDate", new Date());
         return "admin/welcome.html";
     }
 
